da74e3fa59463369c36705e18518c07fa2113b71
Sudipta Karmakar <sudiptacse.seu@gmail.com>
2019-01-30 17:43:52 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Phone number authentication with firebase
---
+++ app/src/main/java/com/example/sudip/phone_number_auth/CountryData.java
@@ -0,0 +1,9 @@
+package com.example.sudip.phone_number_auth;
+
+public class CountryData {
+
+    public static final String[] countryNames = {
+            "Bangladesh"};
+    public static final String[] countryAreaCode = {
+            "880"};
+}
